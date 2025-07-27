0845a39e0b2b7a25532c2e799063bcb11cec0c32
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2025-07-27 11:48:30 +0200

    Add unit tests

---
+++ src/test/java/com/example/demo/GitControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+@WebMvcTest(GitController.class)
+public class GitControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    MockMvcTester tester;
+
+    @Test
+    void shouldReturnRepositories(){
+
+    }
+}
---
+++ src/test/java/com/example/demo/RepositoryServiceTest.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+import com.example.demo.Entity.Branch;
+import com.example.demo.Entity.GithubApiBranch;
+import com.example.demo.Entity.GithubApiCommit;
+import com.example.demo.Entity.GithubApiRepository;
+import com.example.demo.Services.GithubApiService;
+import com.example.demo.Services.RepositoryService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class RepositoryServiceTest {
+
+    @Mock
+    private GithubApiService githubApiService;
+
+    @InjectMocks
+    private RepositoryService repositoryService;
+
+    @Test
+    void shouldReturnRepositoryWithBranches() {
+
+        var commit = new GithubApiCommit("sha");
+        var githubApiRepository = new GithubApiRepository("Repository branchName", false);
+        var githubApiBranch = new GithubApiBranch("Branch name", commit) ;
+
+        when(githubApiService.getUserRepositories("username"))
+                .thenReturn(List.of(githubApiRepository));
+        when(githubApiService.getBranch("username", "Repository branchName"))
+                .thenReturn(List.of(githubApiBranch));
+
+        var result = repositoryService.getRepositoryWithBranches("username");
+
+        assertEquals(1, result.size());
+        assertEquals("username", result.get(0).login());
+        assertEquals("Repository branchName", result.get(0).repositoryName());
+        assertEquals("Branch name",result.get(0).branches().get(0).branchName());
+        assertEquals("sha",result.get(0).branches().get(0).sha());
+    }
+
+    @Test
+    void shouldFilterOutForks(){
+
+        var githubApiRepository = new GithubApiRepository("Repository branchName", true);
+        when(githubApiService.getUserRepositories("username"))
+                .thenReturn(List.of(githubApiRepository));
+
+        var result = repositoryService.getRepositoryWithBranches("username");
+        assertEquals(0,result.size());
+    }
+}
