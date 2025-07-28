4a01d189bec0875b59e4278801d5f34ca1aa7a55
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2025-07-28 21:01:23 +0200

    Add test for GitController

--- src/test/java/com/example/demo/GitControllerTest.java
+++ src/test/java/com/example/demo/GitControllerTest.java
@@ -1,10 +1,22 @@
 package com.example.demo;
 
+import com.example.demo.Entity.Branch;
+import com.example.demo.Entity.Repository;
+import com.example.demo.Services.RepositoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(GitController.class)
 public class GitControllerTest {
@@ -12,10 +24,20 @@ public class GitControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    MockMvcTester tester;
+    @MockitoBean
+    private RepositoryService repositoryService;
+
 
     @Test
-    void shouldReturnRepositories(){
+    void shouldReturnRepositories() throws Exception {
+        Branch branch = new Branch("Branch name", "sha");
+        Repository repository = new Repository("username", "Repository name", List.of(branch));
 
+        when(repositoryService.getRepositoryWithBranches("username")).thenReturn(List.of(repository));
+
+        mockMvc.perform(get("/?username=username").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].login").value("username"));
     }
 }
